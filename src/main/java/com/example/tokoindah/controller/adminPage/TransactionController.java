a991cd5c546f2c93d026485e4e423cf8ed7093af
divanamecca <nadivatv@gmail.com>
2025-06-07 00:59:19 +0700

    1. build page display pelanggan dan transaksi, 2. create field di controller pelanggan dan transaksi

--- src/main/java/com/example/tokoindah/controller/adminPage/TransactionController.java
+++ src/main/java/com/example/tokoindah/controller/adminPage/TransactionController.java
@@ -1,4 +1,18 @@
 package com.example.tokoindah.controller.adminPage;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
 public class TransactionController {
+    public TextField search_field;
+    public Button search_btn;
+    public TableView transaksiTable;
+    public TableColumn nomor_transaksi;
+    public TableColumn tanggal_transaksi;
+    public TableColumn nama_pelanggan;
+    public TableColumn total_transaksi;
+    public TableColumn pembayaran_transaksi;
+    public TableColumn kembalian_transaksi;
 }
