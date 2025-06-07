3ec9e6226bcac7fc46a10066992493aef20c98c9
sellolintang <sellolintang242@gmail.com>
2025-06-07 11:31:02 +0700

    show keranjang

--- src/main/java/com/example/tokoindah/repository/KeranjangRepository.java
+++ src/main/java/com/example/tokoindah/repository/KeranjangRepository.java
@@ -1,43 +1,65 @@
 package com.example.tokoindah.repository;
 import com.example.tokoindah.database.Database;
-import com.example.tokoindah.model.Keranjang;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Random;
-import java.util.Date;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import com.example.tokoindah.model.Keranjang;
 
 public class KeranjangRepository extends Database {
-
     public void createKeranjang(int nomor_transaksi, String kode_produk, String nama_produk, float harga, int quantity, float subtotal) {
         try {
-            String sql = "INSERT INTO keranjang (id_keranjang, nomor_transaksi, kode_produk, nama_produk, harga, quantity, subtotal) VALUES (?, ?, ?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO keranjang (nomor_transaksi, kode_produk, nama_produk, harga, qty, subtotal) VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement ps = conn.prepareStatement(sql);
-            ps.setString(1, generateIdKeranjang());
-            ps.setInt(2, nomor_transaksi);
-            ps.setString(3, kode_produk);
-            ps.setString(4, nama_produk);
-            ps.setFloat(5, harga);
-            ps.setInt(6, quantity);
-            ps.setFloat(7, subtotal);
+            ps.setInt(1, nomor_transaksi);
+            ps.setString(2, kode_produk);
+            ps.setString(3, nama_produk);
+            ps.setFloat(4, harga);
+            ps.setInt(5, quantity);
+            ps.setFloat(6, subtotal);
+            ps.executeUpdate();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
     }
 
-    public static String generateIdKeranjang() {
-        int idAcak = generateRandomId(4);
-        return "KRJ" + idAcak;
-    }
+    public ArrayList<Keranjang> showKeranjang(int nomor_transaksi) {
+        ArrayList<Keranjang> keranjangList = new ArrayList<>();
 
-    public static int generateRandomId(int panjang) {
-        Random rand = new Random();
-        return rand.nextInt(9000) + 1000;
+        try {
+            String sql = "SELECT * FROM keranjang WHERE nomor_transaksi = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, nomor_transaksi);
+
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                Keranjang keranjang = new Keranjang(
+                        rs.getInt("id_keranjang"),
+                        rs.getString("nomor_transaksi"),
+                        rs.getString("kode_produk"),
+                        rs.getString("nama_produk"),
+                        rs.getFloat("harga"),
+                        rs.getInt("qty"),
+                        rs.getFloat("subtotal")
+                );
+                keranjangList.add(keranjang);
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return keranjangList;
     }
 
     public static void main(String[] args) {
         KeranjangRepository keranjangRepository = new KeranjangRepository();
-        keranjangRepository.createKeranjang(12, "PRDK12", "sepatu", 10000, 3, 3000);
+        keranjangRepository.createKeranjang(12, "PRDK32", "buku", 20000, 3, 60000);
+        keranjangRepository.createKeranjang(12, "PRDK22", "tas", 50000, 2, 10000);
+
+        ArrayList<Keranjang> keranjangList = keranjangRepository.showKeranjang(12);
     }
 }
